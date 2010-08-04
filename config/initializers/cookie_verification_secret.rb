17e35576f051be535aaba6fc118db38ddcc81914
LaSombra <lasombra@truta.org>
2010-08-03 23:48:52 -0300

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'ec08986560857024442311db05245fe4cb1ed20e2011d32d33fe37ccd365d7c2c7bda98cc691189126981877ee26fbd2f9da45d5f3ba3c2e49c734fbe254cd2e';
