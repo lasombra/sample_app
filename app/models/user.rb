3c2f7a6e8c9ef38aeb5f875c5034c0abbbfe8f7b
LaSombra <lasombra@truta.org>
2010-08-19 20:14:29 -0300

    User model with password, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,65 @@
 # == Schema Information
-# Schema version: 20100813230858
+# Schema version: 20100818183607
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
-  
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates_presence_of :name, :email
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates_confirmation_of :password
+
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of :password, :within => 8..40
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password? (submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+	user.name											"Ingo Hoffmann"
+	user.email										"ihoffmann@exemplo.com.br"
+	user.password								"abc123de"
+	user.password_confirmation	"abc123de"
+end
