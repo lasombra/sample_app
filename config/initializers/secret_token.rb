54bb46dea7ba00b5e23097b18cea884ea9a249be
LaSombra <lasombra@truta.org>
2010-11-18 17:10:56 -0200

    Converted to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppV3::Application.config.secret_token = '386c561d489a8fb2c81b09474703bdafe6f796daedc8f67f4c79b7abd263605fd3099bd8742e5af51fa01aca4b2001614e217a1924d3d44f47d0ac8c53d29263'
